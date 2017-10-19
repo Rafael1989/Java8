0d68a4c4f132934cde524fd63d367be68fa8b6f2
Intera-PC <Intera-PC@180.239.192.95>
2017-10-19 15:15:55 -0200

    LocalDate DateTimeFormatter Period...

--- java8/src/java8/LocalDateTest.java
+++ java8/src/java8/LocalDateTest.java
@@ -1,10 +1,22 @@
 package java8;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateTest {
 	public static void main(String[] args) {
 		LocalDate hoje = LocalDate.now();
 		System.out.println(hoje);
+		
+		LocalDate outroDia = LocalDate.of(2099, Month.JANUARY, 25);
+		System.out.println(outroDia);
+		
+		Period periodo = Period.between(hoje, outroDia);
+		System.out.println(periodo.getYears() + " anos " + periodo.getMonths() + " meses " + periodo.getDays() + " dias");
+		
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(hoje.format(formatador));
 	}
 }
